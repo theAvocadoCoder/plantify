ef887e8c225337a06016c536119ef6db700e6a39
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-02-09 16:58:03 +0100

    feat: Update store when file is uploaded

--- src/pages/Home/components/UploadBox/index.js
+++ src/pages/Home/components/UploadBox/index.js
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import style from "./upload_box.module.scss";
 import uploadIcon from "../../assets/upload-icon.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { setFile } from "../uploadFileSlice";
 
 const UploadBox = () => {
-  const [file, setFile] = useState(null);
+  const fileState = useSelector((state) => (state.file));
+  const dispatch = useDispatch();
+
+
 
   function handleDragOver (event) {
     event.preventDefault();
@@ -11,24 +16,26 @@ const UploadBox = () => {
 
   function handleDrop(event) {
     event.preventDefault();
-    handleOnSelectFile(Array.from(event.dataTransfer.files)[0]);
+    handleOnSelectFile({ target: event.dataTransfer });
   }
 
-  function handleOnSelectFile(file) {
-    setFile(file);
+  function handleOnSelectFile(event) {
+    const { files } = event.target;
+    const localImageURL = window.URL.createObjectURL(files[0]);
+    dispatch(setFile(localImageURL));
   }
 
   return (
     <div className={style["upload-box"]}>
       <div onDragOver={handleDragOver} onDrop={handleDrop}>
         <img src={uploadIcon} alt="" />
-        <input type="file" id="plant-image" hidden name="plant-image" onChange={(event) => { handleOnSelectFile(Array.from(event.target.files)[0]) }} />
+        <input type="file" id="plant-image" hidden name="plant-image" onChange={handleOnSelectFile} />
         <p>
-          <button disabled={file?false:true}><label htmlFor="plant-image">Choose</label></button>
+          <button disabled={fileState.file ? false : true}><label htmlFor="plant-image">Choose</label></button>
            &nbsp;a file or drag and drop to upload
         </p>
         <p>Upload JPG, PNG or PDF File, max 10MB</p>
-        <button disabled={file?false:true}><label htmlFor="plant-image">Choose Files</label></button>
+        <button disabled={fileState.file ? false : true}><label htmlFor="plant-image">Choose Files</label></button>
       </div>
     </div>
   )
