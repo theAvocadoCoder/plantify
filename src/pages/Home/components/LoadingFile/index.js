118bf000363ce03abd2078df650c1cfbc881d8b5
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-02-16 11:51:28 +0100

    feat: Make API call to identify plant
    - (feat) Hide action buttons when file is not loaded and after plant is
    identified
    - Make API call requesting plant details

--- src/pages/Home/components/LoadingFile/index.js
+++ src/pages/Home/components/LoadingFile/index.js
@@ -5,15 +5,61 @@ import CloseIcon from "@mui/icons-material/Close";
 import ProgressBar from "../../../../components/ProgressBar"
 import styles from "../LoadingFile/loading_file.module.scss";
 import plantImg from "../../../../assets/plant-icon.png"
+import axios from "axios";
 
 const LoadingFile = () => {
 	const [progress, setProgress] = useState(10);
+	const [isIdentified, setIsIdentified] = useState(false);
 	const { file, name, size } = useSelector((state) => state.fileReducer);
 	const dispatch = useDispatch();
 
 	function handleCloseFile() {
 		dispatch(closeFile());
 		setProgress(10);
+		if (isIdentified) setIsIdentified(false);
+	}
+
+	async function makePostToPlantChecker() {
+		await axios
+			.post("https://api.plant.id/v2/identify", {
+				images: [file],
+				plant_details: [
+					"common_names",
+					"wiki_description",
+					"name_authority",
+					"url",
+					"watering",
+					"wiki_image"
+				]
+			}, {
+				headers: {
+					"Api-Key": "QVEdLt0ooP7VIzmPSZo3gXz8Y32IsBLyn2z1fSpjxIl9m2sRGZ"
+				}
+			})
+			.then((response) => {
+				setIsIdentified(true);
+				console.log("this is the response data: ", response.data.suggestion?.length > 1 ? response.data.suggestion[0] : response.data);
+				console.log("this is the request: ", response.request);
+			})
+			.catch((error) => {
+				if (error.response) {
+					console.log("error response data: ", error.response.data);
+					console.log("error response status: ", error.response.status);
+					console.log("error response headers: ", error.response.headers);
+				} else if (error.request) {
+					console.log("error request: ", error.request);
+				} else {
+					console.log('error message: ', error.message);
+				}
+				console.log("error config: ", error.config);
+			})
+
+	}
+
+	function handleIdentifyPlant() {
+		if (file) {
+			makePostToPlantChecker();
+		}
 	}
 
   return (
@@ -56,9 +102,9 @@ const LoadingFile = () => {
 							</div>
 						) : (<></>)
 					}
-					<div className={styles.bottom__layout}>
+					<div className={`${styles.bottom__layout} ${isIdentified | progress < 100 ? styles.hidden : ""}`}>
 						<button className={styles.btn__cancel} onClick={handleCloseFile}>Cancel</button>
-						<button className={styles.btn__proceed}>Identify</button>
+						<button className={styles.btn__proceed} onClick={handleIdentifyPlant}>Identify</button>
 					</div>
 				</section>
 			</div>
