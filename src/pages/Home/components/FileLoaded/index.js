d1595fb9fee2d846e307fda6c33efa3790db5bf8
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-02-18 14:37:00 +0100

    feat: Render the plant details when API responds
    - (chore) Wrap the rendered div in a ternary operator
    - (chore) Format plant details before render
    - (style) Make license citation details smaller than normal text

--- src/pages/Home/components/FileLoaded/index.js
+++ src/pages/Home/components/FileLoaded/index.js
@@ -1,34 +1,60 @@
 import React from "react";
-import sunflower from "../../assets/sunflower.png";
+import { useSelector } from "react-redux";
 import style from "./file_loaded.module.scss";
 
 const FileLoaded = () => {
+  const { plantDetails } = useSelector((state) => state.plantReducer);
+
+  function capitalize(word) {
+    let wordArray = word.split("");
+    wordArray.splice(0, 1, wordArray[0].toUpperCase());
+    return wordArray.join("");
+  }
+  
   return (
-    true ? (
-      <div className={style["file-loaded__div"]}>
+    <div className={style["file-loaded__div"]}>
+      {plantDetails ? (
         <div>
           <p className={style.h2}>Plant Information</p>
-          <img src={sunflower} alt="a sunflower" title="blah blah blah" />
+          <div>
+            <img src={plantDetails?.plant_details?.wiki_image.value} alt={`a ${plantDetails?.plant_details?.common_names[0]}`} />
+            <span>
+              Citation: {plantDetails?.plant_details?.wiki_image.citation}
+              <br />
+              License: <a href={plantDetails?.plant_details?.wiki_image.license_url}>
+                { plantDetails?.plant_details?.wiki_image.license_name }
+              </a>
+            </span>
+          </div>
           <p className={style.h3}>
             Common Name: &nbsp;
-            <span>Sunflower</span>
+            <span>{capitalize(plantDetails?.plant_details?.common_names[0])}</span>
           </p>
           <p className={style.h3}>
             Scientific Name: &nbsp;
-            <span>Helianthus annuus</span>
+            <span>{plantDetails?.plant_name}</span>
           </p>
           <p className={style.h3}>
             Probability: &nbsp;
-            <span>20%</span>
+            <span>{Math.round(plantDetails?.probability * 100)}%</span>
           </p>
           <p className={style.h3}>Plant Details</p>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero earum, assumenda voluptatum deleniti, eveniet sapiente aperiam minus ad porro ab recusandae. Culpa architecto voluptatum iusto laborum voluptas itaque ea suscipit.
+            {plantDetails?.plant_details?.wiki_description.value}
+            <br />
+            <span>
+              Cited from: <a href={plantDetails?.plant_details?.wiki_description.citation}>
+                Wikipedia
+              </a>
+              <br />
+              License: <a href={plantDetails?.plant_details?.wiki_description.license_url}>
+                { plantDetails?.plant_details?.wiki_description.license_name }
+              </a>
+            </span>            
           </p>
-          <span>For further reading, checkout <a href="/">this link</a></span>
-        </div>
-      </div>
-    ) : (<></>)
+          <span>For further reading, checkout <a href={plantDetails?.plant_details?.url}>this link</a></span>
+        </div>) : (<></>)}
+    </div>
   )
 }
 
